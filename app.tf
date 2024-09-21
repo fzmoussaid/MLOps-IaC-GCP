d74a3ac0f823e258e6dbdde88fa95fe1e7e987d0
fzmoussaid <fatima.zahra.moussaid94@gmail.com>
2024-09-21 21:45:53 +0900

    Add k8s cluster with network subnetwork + deployment + service

---
+++ app.tf
@@ -0,0 +1,115 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.vegetables-cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.vegetables-cluster.master_auth[0].cluster_ca_certificate)
+
+}
+
+resource "kubernetes_deployment_v1" "deploy_vegetables" {
+  metadata {
+    name = "vegetable-deployment"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "classifier-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "classifier-app"
+        }
+      }
+
+      spec {
+        container {
+          image = "${var.region}-docker.pkg.dev/${var.project_name}/model-deploy/vegetables:v1"
+          name  = "classifier-app-container"
+
+          port {
+            container_port = 8080
+            name           = "vegetables-svc"
+          }
+
+          security_context {
+            allow_privilege_escalation = false
+            privileged                 = false
+            read_only_root_filesystem  = false
+
+            capabilities {
+              add  = []
+              drop = ["NET_RAW"]
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = "vegetables-svc"
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+
+        security_context {
+          run_as_non_root = true
+
+          seccomp_profile {
+            type = "RuntimeDefault"
+          }
+        }
+
+        # Toleration is currently required to prevent perpetual diff:
+        # https://github.com/hashicorp/terraform-provider-kubernetes/pull/2380
+        toleration {
+          effect   = "NoSchedule"
+          key      = "kubernetes.io/arch"
+          operator = "Equal"
+          value    = "amd64"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "service_vegetables" {
+  metadata {
+    name = "vegetables-classifier-app-loadbalancer"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.deploy_vegetables.spec[0].selector[0].match_labels.app
+    }
+
+    ip_family_policy = "RequireDualStack"
+
+    port {
+      port        = 80
+      target_port = kubernetes_deployment_v1.deploy_vegetables.spec[0].template[0].spec[0].container[0].port[0].name
+    }
+
+    type = "LoadBalancer"
+  }
+
+  depends_on = [time_sleep.wait_service_cleanup]
+}
+
+# Provide time for Service cleanup
+resource "time_sleep" "wait_service_cleanup" {
+  depends_on = [google_container_cluster.vegetables-cluster]
+
+  destroy_duration = "180s"
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+
+variable "project_name" {
+  type    = string
+  default = "your-project-name"
+}
+
+
+variable "region" {
+  type    = string
+  default = "your-region"
+}
